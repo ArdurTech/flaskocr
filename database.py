256f14c6b22a417c72433e6b08b0380ca472495c
adityapurohit3110 <adiraj3110@gmail.com>
2024-09-05 11:11:36 +0530

    Registration page added

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+import bcrypt
 import pymysql
 from flask import current_app
 
@@ -36,6 +37,14 @@ def create_database_and_table():
             extracted_text TEXT
         )
     """)
+    
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS user (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            username VARCHAR(255) UNIQUE,
+            password_hash VARCHAR(255)
+        )
+    """)
 
     connection.commit()
     cursor.close()
@@ -53,4 +62,27 @@ def insert_data(filepath, inputs, extracted_text):
     connection.commit()
     cursor.close()
     connection.close()
+    
+def get_user_by_username(username):
+    connection = get_db_connection()
+    cursor = connection.cursor(pymysql.cursors.DictCursor)
+    cursor.execute("SELECT username, password_hash FROM user WHERE username = %s", (username,))
+    user = cursor.fetchone()
+    cursor.close()
+    connection.close()
+    return user
+
+def create_user(username, password):
+    hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    connection = get_db_connection()
+    cursor = connection.cursor()
+
+    try:
+        cursor.execute("INSERT INTO user (username, password_hash) VALUES (%s, %s)", (username, hashed.decode('utf-8')))
+        connection.commit()
+    except pymysql.MySQLError as e:
+        print(f"Error: {e}")
+    finally:
+        cursor.close()
+        connection.close()
 
